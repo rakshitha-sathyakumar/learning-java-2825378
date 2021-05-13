ec1cbf7a5ada77b130980ebc50a0fa70f42c4a5f
rakshitha-sathyakumar <rakshu595@gmail.com>
2021-05-13 15:57:43 -0700

    Initial programs

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int studentAge = 15;
+        double studentGPA = 3.56;
+        char firstInitial = 'R';
+        char lastInitial = 'S';
+        boolean hasPerfectAttendance = true;
+        String studentName = "Rakshitha";
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(firstInitial);
+        System.out.println(studentName);
+    }
+}
