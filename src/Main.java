c19844704a84f11cb454f05aff1a9f1821ab173d
rakshitha-sathyakumar <rakshu595@gmail.com>
2021-05-13 16:06:02 -0700

    Concatination in java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         double studentGPA = 3.45;
-        String studentFirstName = "Kayla";
-        String studentLastName = "Hammon";
+        String studentFirstName = "Rakshitha";
+        String studentLastName = "Sathyakumar";
+        System.out.println(studentFirstName+ " " +studentLastName + "has a gpa of " + studentGPA);
     }
 }
