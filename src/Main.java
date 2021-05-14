3525105bbeb00d89e32c0334067a1836b14781b1
rakshitha-sathyakumar <rakshu595@gmail.com>
2021-05-14 14:57:52 -0700

    yearly salary

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
 
+    public static double employeeSalary(double hours, double amount) {
+
+        if (hours < 0) {
+            return -1;
+        }
+
+        if (amount < 0) {
+            return -1;
+        }
+        double totalWeek = hours * amount;
+        double totalYear = totalWeek * 52;
+        return totalYear;
+    }
     public static void main(String[] args) {
+
+        double salary = employeeSalary(40, 35);
+        System.out.println(salary);
+
+        double excludingVacation = salary - (8 * 8 * 35);
+        System.out.println(excludingVacation);
         
     }
 
