c36ae8cbf977f145b05ba6401514399bfa46e42c
rakshitha-sathyakumar <rakshu595@gmail.com>
2021-05-14 14:25:40 -0700

    functions with parameters

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
 
-    public static void calculateTotalMealPrice() {
+    public static void calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
+        calculateTotalMealPrice(12, .3, .05);
     }
 
 }
