0d08bdfc948aae8cd18f8ff8e2b2705aff631d28
rakshitha-sathyakumar <rakshu595@gmail.com>
2021-05-14 11:58:26 -0700

    mcq challenge

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,38 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "What is the value of (2+3)?";
+        String choiceOne = "one";
+        String choiceTwo = "five";
+        String choiceThree = "three";
 
         String correctAnswer = choiceTwo;
 
         // Write a print statement asking the question
+        System.out.println(question);
+
         // Write a print statement giving the answer choices
+        System.out.println("Pick the correct choice from the following answers");
+        System.out.println(choiceOne);
+        System.out.println(choiceTwo);
+        System.out.println(choiceThree);
 
         // Have the user input an answer
+
+        Scanner scanner = new Scanner(System.in);
         // Retrieve the user's input
+
+        String userInput = scanner.next();
 
         // If the user's input matches the correctAnswer...
         // then the user is correct and we want to print out a congrats message to the user.
+        if (userInput.toLowerCase().equals(correctAnswer)) {
+            System.out.println("Congrats your answer is correct");
+        } else {
+            System.out.println("Incorrect!! The correct answer is " + correctAnswer);
+        }
 
         // If the user's input does not match the correctAnswer...
         // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
