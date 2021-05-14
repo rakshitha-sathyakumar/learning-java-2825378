2ba464a7e7c1a641f2db6427317e98d437f992c7
rakshitha-sathyakumar <rakshu595@gmail.com>
2021-05-14 14:15:06 -0700

    functions in Java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 public class Main {
 
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer tea time...");
+        System.out.println("Type in a word and press return to start the developer tea time");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's developer tea time");
+    }
     public static void main(String[] args) {
-
+        announceDeveloperTeaTime();
     }
 
 }
