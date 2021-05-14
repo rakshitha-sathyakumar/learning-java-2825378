082eee83be38992af3f5e592ce1111fb31cc5249
rakshitha-sathyakumar <rakshu595@gmail.com>
2021-05-14 11:42:25 -0700

    while loop

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean isOnRepeat = true;
+
+        while (isOnRepeat) {
+            System.out.println("Play the current song");
+            System.out.println("Would you like to change the songs? Yes or No");
+
+            String userInput = input.next();
+
+            if (userInput.equals("Yes")) {
+                isOnRepeat = false;
+            } else {
+                System.out.println("Continue enjoying your current song");
+            }
+        }
+        System.out.println("Playing next song");
     }
 }
