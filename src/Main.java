d2bbb32053bede4fe33072a82c2f70c237f8bac6
rakshitha-sathyakumar <rakshu595@gmail.com>
2021-05-17 12:22:22 -0700

    instances

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+        Trinagle traingleA = new Traingle(15, 8 , 15, 8 , 17);
+        Trinagle traingleB = new Traingle(3, 2.598 , 3, 3, 5);
+        
 
     }
 
